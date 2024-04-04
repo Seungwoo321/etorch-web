0b462222fb3fd360424897f40a409f5559c46463
unknown <seungwoo321@gmail.com>
2024-04-05 00:55:21 +0900

    fix: modify packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import "@/styles/globals.css";
 // import { ThemeProvider } from "@/components/theme-provider"
 // import { ModeToggle } from "@/components/ModeToggle"
-import RootLayout from "./_root/RootLayout";
-import Dashboard from "./_root/pages/Dashboard";
+import RootLayout from "./pages/RootLayout";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
