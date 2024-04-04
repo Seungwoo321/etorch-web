b9e194f5a5646a6b679f0f2b4149df314bd29871
Seungwoo321 <seungwoo321@gmail.com>
2024-04-04 23:44:50 +0900

    refactor: remove any

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -5,10 +5,10 @@ export const http = {
   get: function httpGet<Response = unknown>(url: string) {
     return axios.get<Response>(url).then(res => res.data);
   },
-  post: function httpPost<Request = any, Response = unknown>(url: string, data?: Request) {
+  post: function httpPost<Request, Response = unknown>(url: string, data?: Request) {
     return axios.post<Response>(url, { data }).then(res => res.data);
   },
-  delete: function httpDelete<Request = any, Response = unknown>(url: string, data?: Request) {
+  delete: function httpDelete<Request, Response = unknown>(url: string, data?: Request) {
     return axios.delete<Response>(url, { data }).then(res => res.data);
   },
 };
