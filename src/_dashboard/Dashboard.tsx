c73d6dcbf417b69760e8c37c3b282fadb7bd423c
Seungwoo321 <seungwoo321@gmail.com>
2024-04-29 19:10:06 +0900

    fix: typo

--- src/_dashboard/Dashboard.tsx
+++ src/_dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import CardPreview from "@/_dashboard/card/CardPreview"
 import CardControl from "@/_dashboard/card/CardControl"
 const Dashboard = () => {
   return (
-    <div className="container results={results}py-6">
+    <div className="container py-6">
       <div className="gap-6 grid h-full items-stretch md:grid-cols-[1fr_300px]">
         <CardControl/>
         <CardPreview/>
