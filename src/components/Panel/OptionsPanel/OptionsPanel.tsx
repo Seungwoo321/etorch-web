ccf0b91666bbac0317dee363fb6d791a5478d26c
Seungwoo321 <seungwoo321@gmail.com>
2024-08-25 06:14:59 +0900

    style: add scroll in option panel

--- src/components/Panel/OptionsPanel/OptionsPanel.tsx
+++ src/components/Panel/OptionsPanel/OptionsPanel.tsx
@@ -13,72 +13,74 @@ import SelectionLegendOption from '@/components/Selection/SelectionLegendOption'
 import SelectionXAxisOption from '@/components/Selection/SelectionXAxisOption'
 import SelectionYAxisOption from '@/components/Selection/SelectionYAxisOption'
 import SelectionYAxisSecondaryOption from '@/components/Selection/SelectionYAxisSecondaryOption'
+import { ScrollArea } from '@/components/ui/scroll-area'
 function OptionsPanel (): JSX.Element {
   return (
-    <div className="h-full">
-      <div className="p-1">
-        {/* <VisualizationsComboBox/> */}
-        <VisualizationsSelect/>
-      </div>
-      <div className="h-[calc(100%-60px)]">
-        <div className="flex p-2">
+    <ScrollArea className="h-min-0 rounded-xl">
+      <div className="h-full">
+        <div className="p-1">
+          {/* <VisualizationsComboBox/> */}
+          <VisualizationsSelect/>
+        </div>
+        <div className="h-[calc(100%-60px)]">
+          <div className="flex p-2">
 
-          <Accordion className="w-full" type="single" collapsible>
+            <Accordion className="w-full" type="single" collapsible>
+              <AccordionItem value="panel-options">
+                <AccordionTrigger>
+                  <span>Panel options</span>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <SelectionPanelOption/>
+                </AccordionContent>
+              </AccordionItem>
 
-            <AccordionItem value="panel-options">
-              <AccordionTrigger>
-                <span>Panel options</span>
-              </AccordionTrigger>
-              <AccordionContent>
-                <SelectionPanelOption/>
-              </AccordionContent>
-            </AccordionItem>
+              <AccordionItem value="tooltip">
+                <AccordionTrigger>Tooltip</AccordionTrigger>
+                <AccordionContent>
+                  <SelectionTooltipOption/>
+                </AccordionContent>
+              </AccordionItem>
 
-            <AccordionItem value="tooltip">
-              <AccordionTrigger>Tooltip</AccordionTrigger>
-              <AccordionContent>
-                <SelectionTooltipOption/>
-              </AccordionContent>
-            </AccordionItem>
+              <AccordionItem value="legend">
+                <AccordionTrigger>Legend</AccordionTrigger>
+                <AccordionContent>
+                  <SelectionLegendOption/>
+                </AccordionContent>
+              </AccordionItem>
 
-            <AccordionItem value="legend">
-              <AccordionTrigger>Legend</AccordionTrigger>
-              <AccordionContent>
-                <SelectionLegendOption/>
-              </AccordionContent>
-            </AccordionItem>
+              <AccordionItem value="x-axis">
+                <AccordionTrigger>X-Axis</AccordionTrigger>
+                <AccordionContent>
+                  <SelectionXAxisOption />
+                </AccordionContent>
+              </AccordionItem>
 
-            <AccordionItem value="x-axis">
-              <AccordionTrigger>X-Axis</AccordionTrigger>
-              <AccordionContent>
-                <SelectionXAxisOption />
-              </AccordionContent>
-            </AccordionItem>
+              <AccordionItem value="y-axis">
+                <AccordionTrigger>Y-Axis</AccordionTrigger>
+                <AccordionContent>
+                  <SelectionYAxisOption />
+                </AccordionContent>
+              </AccordionItem>
 
-            <AccordionItem value="y-axis">
-              <AccordionTrigger>Y-Axis</AccordionTrigger>
-              <AccordionContent>
-                <SelectionYAxisOption />
-              </AccordionContent>
-            </AccordionItem>
+              <AccordionItem value="y-axis-scondary">
+                <AccordionTrigger>Y-Axis (Scondary)</AccordionTrigger>
+                <AccordionContent>
+                  <SelectionYAxisSecondaryOption />
+                </AccordionContent>
+              </AccordionItem>
 
-            <AccordionItem value="y-axis-scondary">
-              <AccordionTrigger>Y-Axis (Scondary)</AccordionTrigger>
-              <AccordionContent>
-                <SelectionYAxisSecondaryOption />
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="graph-styles">
-              <AccordionTrigger>Graph styles</AccordionTrigger>
-              <AccordionContent>
-                <SelectionGraphStyle/>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+              <AccordionItem value="graph-styles">
+                <AccordionTrigger>Graph styles</AccordionTrigger>
+                <AccordionContent>
+                  <SelectionGraphStyle/>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
       </div>
-    </div>
+    </ScrollArea>
   )
 }
 
